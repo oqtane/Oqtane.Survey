2a4a4f783b2ccb36d87816078406dc55dbf3eacb
Michael Washington <webmaster@adefwebserver.com>
2021-01-16 10:04:43 -0800

    Adding new table structure

--- Server/Scripts/Oqtane.Survey.Uninstall.sql
+++ Server/Scripts/Oqtane.Survey.Uninstall.sql
@@ -2,5 +2,14 @@
 Remove OqtaneSurvey table
 */
 
+DROP TABLE [dbo].[OqtaneSurveyItemOption]
+GO
+
+DROP TABLE [dbo].[OqtaneSurveyAnswer]
+GO
+
+DROP TABLE [dbo].[OqtaneSurveyItem]
+GO
+
 DROP TABLE [dbo].[OqtaneSurvey]
 GO
