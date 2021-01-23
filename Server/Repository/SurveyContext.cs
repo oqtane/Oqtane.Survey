172f688e9b2fe4b00bead67abe2863bf5a66b3e2
Michael Washington <webmaster@adefwebserver.com>
2021-01-23 08:57:40 -0800

    Updated Data Context

--- Server/Repository/SurveyContext.cs
+++ Server/Repository/SurveyContext.cs
@@ -9,6 +9,9 @@ namespace Oqtane.Survey.Repository
     public class SurveyContext : DBContextBase, IService
     {
         public virtual DbSet<Models.Survey> Survey { get; set; }
+        public virtual DbSet<SurveyAnswer> SurveyAnswer { get; set; }
+        public virtual DbSet<SurveyItem> SurveyItem { get; set; }
+        public virtual DbSet<SurveyItemOption> SurveyItemOption { get; set; }
 
         public SurveyContext(ITenantResolver tenantResolver, IHttpContextAccessor accessor) : base(tenantResolver, accessor)
         {
---
+++ Shared/Classes/DTOSurvey.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oqtane.Survey
+{
+    public class DTOSurvey
+    {
+        public int Id { get; set; }
+        public string SurveyName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public int UserId { get; set; }
+        public List<DTOSurveyItem> SurveyItem { get; set; }
+    }
+    public class DTOSurveyItem
+    {
+        public int Id { get; set; }
+        public string ItemLabel { get; set; }
+        public string ItemType { get; set; }
+        public string ItemValue { get; set; }
+        public int Position { get; set; }
+        public int Required { get; set; }
+        public int? SurveyChoiceId { get; set; }
+        public string AnswerValueString { get; set; }
+        public IEnumerable<string> AnswerValueList { get; set; }
+        public DateTime? AnswerValueDateTime { get; set; }
+        public List<DTOSurveyItemOption> SurveyItemOption { get; set; }
+        public List<AnswerResponse> AnswerResponses { get; set; }
+    }
+    public partial class DTOSurveyItemOption
+    {
+        public int Id { get; set; }
+        public string OptionLabel { get; set; }
+    }
+    public class AnswerResponse
+    {
+        public string OptionLabel { get; set; }
+        public double Responses { get; set; }
+    }
+}
