4d030097f27a0d70d8274c9fb22ebc5258fe62ef
Michael Washington <webmaster@adefwebserver.com>
2021-03-28 10:17:23 -0700

    Increased to Oqtane.Survey.1.1.0

--- Client/ModuleInfo.cs
+++ Client/ModuleInfo.cs
@@ -9,9 +9,9 @@ namespace Oqtane.Survey
         {
             Name = "Survey",
             Description = "Survey",
-            Version = "1.0.0",
+            Version = "1.1.0",
             ServerManagerType = "Oqtane.Survey.Manager.SurveyManager, Oqtane.Survey.Server.Oqtane",
-            ReleaseVersions = "1.0.0",
+            ReleaseVersions = "1.1.0",
             Dependencies = "Oqtane.Survey.Shared.Oqtane,Radzen.Blazor,System.Linq.Dynamic.Core"
         };
     }
