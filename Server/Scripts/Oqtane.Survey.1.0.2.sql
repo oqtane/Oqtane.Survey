c962872e6941e07a397ad616d4ccddef7140f8ba
Michael Washington <webmaster@adefwebserver.com>
2021-05-13 17:35:38 -0700

    Addresses #4 - Make [dbo].[OqtaneSurvey].[UserId] INTEGER NULL and increase version to 1.0.2

---
+++ Server/Scripts/Oqtane.Survey.1.0.2.sql
@@ -0,0 +1,19 @@
+/*  
+  Make [dbo].[OqtaneSurvey].[UserId] INTEGER NULL
+*/
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OqtaneSurvey]') AND type in (N'U'))
+BEGIN
+
+ALTER TABLE [dbo].[OqtaneSurvey] ALTER COLUMN [UserId] INTEGER NULL
+
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
