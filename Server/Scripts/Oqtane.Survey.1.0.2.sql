ef8cf2fe0591d6ddf4c09387374587aa312ce0fe
Michael Washington <webmaster@adefwebserver.com>
2021-05-15 17:33:09 -0700

    Add AnonymousCookie

--- Server/Scripts/Oqtane.Survey.1.0.2.sql
+++ Server/Scripts/Oqtane.Survey.1.0.2.sql
@@ -23,6 +23,14 @@ ALTER TABLE [dbo].[OqtaneSurveyAnswer] ALTER COLUMN [UserId] INTEGER NULL
 END
 GO
 
+IF NOT EXISTS (SELECT * FROM sys.columns WHERE object_id = OBJECT_ID(N'[dbo].[OqtaneSurveyAnswer]') AND name = 'AnonymousCookie')
+BEGIN
+
+ALTER TABLE dbo.OqtaneSurveyAnswer ADD AnonymousCookie nvarchar(500) NULL
+
+END
+GO
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
